c34933d298e2f10780897bd9f4c59613aae41cbf
Shanthi Sree Devaraju <shanthisree0315@gmail.com>
2022-10-21 15:58:05 -0400

    flake8 fixes

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,6 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-from django.contrib import admin
 
 
 class Question(models.Model):
--- polls/tests.py
+++ polls/tests.py
@@ -40,15 +40,15 @@ class QuestionModelTests(TestCase):
 
 class QuestionIndexViewTests(TestCase):
 
-    
+
     def create_question(question_text, days):
-    """
-    Create a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+        """
+        Create a question with the given `question_text` and published the
+        given number of `days` offset to now (negative for questions published
+        in the past, positive for questions that have yet to be published).
+        """
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
 
     def test_no_questions(self):
         """
